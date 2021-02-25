e6a169561c339a0a8d502f3acd8a1542b86535d9
Digimonsoul <70336998+Digimonsoul@users.noreply.github.com>
2021-02-25 14:04:20 -0500

    Finished Species Select

---
+++ src/com/company/Character.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.LinkedList;
+
+/**
+ * Created by ew7100 on 2/25/2021.
+ */
+public class Character {
+
+    String Name;
+    String Species;
+    String Gender;
+    String Element;
+    LinkedList<Inventory> CharacterInventory = new LinkedList<>();
+
+    public void Character(String Name, String Species, String Gender,
+                          String Element){
+        this.Name=Name;
+        this.Species=Species;
+        this.Gender=Gender;
+        this.Element=Element;
+    }
+
+}
